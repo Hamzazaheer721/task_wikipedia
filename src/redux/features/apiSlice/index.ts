0af7ba948fd0751b70b4a804b620e4e7e0a03440
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-12-07 01:33:52 +0500

    Update index.ts

--- src/redux/features/apiSlice/index.ts
+++ src/redux/features/apiSlice/index.ts
@@ -7,4 +7,4 @@ export const apiSlice = createSlice({
   reducers: {}
 })
 
-// this slice could not be continued due to shortage of time
+// this slice could not be continued due to shortage of time so moved the api logic to custom hook and worked around in order to save time
